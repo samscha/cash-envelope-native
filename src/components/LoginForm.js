202c262639bd567632ac4a8ee3b087e57542db7d
Samuel Cha <sunchipssam@gmail.com>
2018-09-14 16:17:56 -0700

    Added responsive ui and alerts

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Alert,
   AsyncStorage,
   StyleSheet,
   Text,
@@ -11,62 +12,23 @@ import {
 import axios from '../axios';
 
 export default class LoginForm extends React.Component {
-  state = {
-    username: '',
-    password: '',
-  };
+  state = { username: '', password: '', isLoggingIn: false };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          onSubmitEditing={() => this.passwordInput.focus()}
-          autoCorrect={false}
-          keyboardType="email-address"
-          returnKeyType="next"
-          placeholder="Email"
-          underlineColorAndroid="transparent"
-          onChangeText={text => this.setState({ username: text })}
-        />
-
-        <TextInput
-          style={styles.input}
-          returnKeyType="go"
-          ref={input => (this.passwordInput = input)}
-          placeholder="Password"
-          onChangeText={text => this.setState({ password: text })}
-          underlineColorAndroid="transparent"
-          secureTextEntry
-        />
-
-        <TouchableOpacity
-          style={styles.buttonContainer}
-          onPress={_ => this._logInAsync()}
-        >
-          <Text style={styles.buttonText}>Log in</Text>
-        </TouchableOpacity>
-      </View>
-    );
+  componentDidMount() {
+    this.emailInput.focus();
   }
 
-  _logInAsync = async () => {
-    if (!this.state.username && !this.state.password) {
-      return alert('Please provide an email and password');
-    }
+  _logInAsync = async _ => {
+    const username = this.state.username;
+    const password = this.state.password;
 
-    if (this.state.username === '' || this.state.password === '') {
-      if (!this.state.username) {
-        return alert('Please provide an email');
-      }
-
-      return alert('Please provide a password');
-    }
+    if (!this._validateInput(username, password)) return;
 
     try {
+      this.setState({ isLoggingIn: true });
       const response = await axios.post('/login', {
-        ...this.state,
+        username,
+        password,
       });
 
       await AsyncStorage.setItem(
@@ -76,9 +38,123 @@ export default class LoginForm extends React.Component {
 
       this.props.navigation.navigate('App');
     } catch (error) {
-      alert(error.response.data.message);
+      /**
+       * if there is no response error, this means a response was never received
+       * this is likely due to connectivity issues
+       *
+       */
+      const title = 'Error';
+
+      const msg = error.response
+        ? error.response.data.message
+        : error.request._response;
+
+      this._renderAlert(title, msg, _ => {
+        this.setState({ isLoggingIn: false });
+
+        if (msg.includes('username')) return this.emailInput.focus();
+
+        if (msg.includes('password')) return this.passwordInput.focus();
+      });
     }
   };
+
+  _renderAlert = (title, msg, cb) => {
+    Alert.alert(title, msg, [
+      {
+        text: 'OK',
+        onPress: _ => cb(),
+      },
+    ]);
+  };
+
+  /**
+   * `state`ful styles
+   */
+  _styles = key => {
+    const styles = { opacity: this.state.isLoggingIn ? 0.2 : 1.0 };
+
+    return styles[key];
+  };
+
+  _validateInput = (username, password) => {
+    if (!username && !password) {
+      return Alert.alert('', 'Please provide an email and password', [
+        {
+          text: 'OK',
+          onPress: _ => this.emailInput.focus(),
+        },
+      ]);
+    }
+
+    if (username === '' || password === '') {
+      if (!username) {
+        return Alert.alert('', 'Please provide an email', [
+          {
+            text: 'OK',
+            onPress: _ => this.emailInput.focus(),
+          },
+        ]);
+      }
+
+      return Alert.alert('', 'Please provide a password', [
+        {
+          text: 'OK',
+          onPress: _ => this.passwordInput.focus(),
+        },
+      ]);
+    }
+
+    return true;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          editable={!this.state.isLoggingIn}
+          keyboardType="email-address"
+          onChangeText={text => this.setState({ username: text })}
+          onSubmitEditing={_ => this.passwordInput.focus()}
+          placeholder="Email"
+          ref={input => (this.emailInput = input)}
+          returnKeyType="next"
+          style={[styles.input, { opacity: this._styles('opacity') }]}
+          underlineColorAndroid="transparent"
+        />
+
+        <TextInput
+          editable={!this.state.isLoggingIn}
+          style={[styles.input, { opacity: this._styles('opacity') }]}
+          returnKeyType="go"
+          ref={input => (this.passwordInput = input)}
+          placeholder="Password"
+          onChangeText={text => this.setState({ password: text })}
+          onSubmitEditing={_ => this._logInAsync()}
+          underlineColorAndroid="transparent"
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          disabled={this.state.isLoggingIn}
+          onPress={_ => this._logInAsync()}
+          style={[styles.buttonContainer, { opacity: this._styles('opacity') }]}
+        >
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          disabled={!this.state.isLoggingIn}
+          style={[styles.signUpContainer, { opacity: this._styles('opacity') }]}
+          onPress={_ => this.props.navigation.navigate('Signup')}
+        >
+          <Text style={styles.signUpText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -97,6 +173,17 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  signUpContainer: {
+    marginTop: 10,
+    paddingVertical: 15,
+    borderColor: '#2980b6',
+    borderWidth: 1,
+  },
+  signUpText: {
+    color: '#2980b6',
     textAlign: 'center',
     fontWeight: '700',
   },
